5bae8aa31040dae662e2d9d41c6c06256126b9af
Raven Williams <heatherwilliams.raven@gmail.com>
2022-09-10 21:53:44 -0500

    Created the functionality to update products in the cars app views file

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -23,19 +24,18 @@ def product_list(request):
         
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
-@api_view(['GET'])
+@api_view(['GET','PUT'])
 def products_detail(request, pk):
+    products = get_object_or_404(Products, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductsSerializer(products);
+        return Response(serializer.data)
 
-        try:
-
-            products = Products.objects.get(pk=pk)
-
-            serializer = ProductsSerializer(products)
-
-            return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductsSerializer(products, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
         
-        except Products.DoesNotExist:
 
-            return Response(status=status.HTTP_404_NOT_FOUND);
-        
-        # return Response(pk)
+
