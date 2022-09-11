633311c07ba20cd11c0bb0c424cc7fd23dd8168e
Raven Williams <heatherwilliams.raven@gmail.com>
2022-09-10 19:44:44 -0500

    Created admin log in

--- products/views.py
+++ products/views.py
@@ -1,7 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+import products
+from .serializers import ProductsSerializer
+from .models import Products
 @api_view(['GET'])
 def product_list(request):
 
-    return Response('ok')
+    products = Products.objects.all()
+
+    serializer = ProductsSerializer(products, many=True)
+
+    return Response(serializer.data)
